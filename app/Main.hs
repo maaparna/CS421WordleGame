142db2d19a1ce52735395c19d51b2235ad27e06d
maaparna <48700230+maaparna@users.noreply.github.com>
2023-07-30 17:08:17 +1200

    latest

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,6 @@ import WordleGame
 -- main :: IO ()
 -- main = putStrLn "Hello, Haskell!"
 
+-- Call the WordleGame with a value greater than 1 to get into the game mode
 main :: IO [Int]
 main = wordleGameMode 1
--- lib/WordleGame.hs
+++ lib/WordleGame.hs
@@ -18,16 +18,22 @@ import Data.List ( intersect )
 import Data.String (String)
 import Text.ParserCombinators.ReadPrec (reset)
 import System.Exit ( exitFailure )
---import System.Console.ANSI
 
+--Total number of attempts allowed per round
 attempts :: Int
 attempts = 6
 
--- strConv :: String -> Text
--- strConv s = pack s
+--The main function that compares the entered word with the selected word.
+--It marks the letters that are in the correct place with green background
+--It marks the letters that is in incorrect place within the word length with yellow background
+--It marks the letters that are not part of the correct word and the letters
+--that are more than the word length with black background
 
--- textConv :: Text -> String
--- textConv s = unpack s
+--The function returns a Text object (word with the color background) and a bool flag 
+--to indicate if the word attempted matches the correct word
+
+--The coloring is done by the go function in the where clause which is a recursive function.
+--It loops through each letter of the attempted word to determine the background colour
 
 getWordle :: Text -> Text -> (Text, Bool)
 getWordle attempt correct =
@@ -46,7 +52,15 @@ getWordle attempt correct =
       | w == u = '\ESC':'[':'4':'2':'m':w:'\ESC':'[':'0':'m': go ws us intersection
       | w `elem` intersection = '\ESC':'[':'1':'0':'3':'m':w:'\ESC':'[':'0':'m': go ws us intersection
       | otherwise = '\ESC':'[':'4':'0':'m':u:'\ESC':'[':'0':'m': go ws us intersection
+
+--These are SCC (Set Cost Center) to help with performance profiling of the getWordle function
 {-# SCC getWordle #-}
+
+
+-- This is a recursive function that allos the user to guess the word to the max of "attempts" times
+--It calls the getWordle function for each entry and prints out the word with the colored background to
+--proviude hint to the user
+
 wordleGame :: Text ->Int-> IO [Text]
 wordleGame selected_word word_length = go attempts []
   where
@@ -58,24 +72,11 @@ wordleGame selected_word word_length = go attempts []
     go n xs = do
       let i = 1 + Prelude.length xs
 
-      --putStrLn $ "Please enter your " ++ show word_length ++ " letters: "
-
       attemptstr <- getLine
       let attemp = toLower . strip $ pack attemptstr
       let (wordle, correct) = getWordle attemp selected_word
 
       printf "you have used %d/%d attempts\n\n" i attempts
-    --   printf "you \n\n"
-
-    --   let w = strConv ['\ESC', '[', '4', '0', 'm', 'p', '\ESC', '[', '0', 'm', '\ESC', '[', '4', '2', 'm', 'i', '\ESC', '[', '0', 'm', '\ESC', '[',
-    --           '4', '0', 'm', 'g', '\ESC', '[', '0', 'm']
-
-    --   TIO.putStrLn w
-
-    --   printf "you \n\n"
-
-
-      --TIO.putStrLn . T.unlines . reverse $ (wordle : xs)
 
       TIO.putStrLn wordle
       printf "\n\n"
@@ -87,8 +88,14 @@ wordleGame selected_word word_length = go attempts []
           return (wordle : xs)
         else do
           go (n - 1) (wordle : xs)
+
+--These are SCC (Set Cost Center) to help with performance profiling of the wordleGame function
 {-# SCC wordleGame #-}
 
+--This function uses a random number generator to pick a ord from the file list and use that as the 
+--word for the user to guess
+--Once the word is selected a prompt is provided for the user to enter a word of the expected length
+--The selected word and its length are sent as arguments to the wordleGame function to start the counter on user attempts
 getWord :: String -> IO ()
 getWord filename = do
     wordleEText <- readFile filename
@@ -108,25 +115,38 @@ getWord filename = do
     wordlesGE <- wordleGame selected_wordGE wordGELength
 
     TIO.putStrLn . T.unlines . reverse $ wordlesGE
+
+--These are SCC (Set Cost Center) to help with performance profiling of the getWord function
 {-# SCC getWord #-}
 
 
-
+--the filename for the easy mode
 easyWordleGameMode:: IO ()
 easyWordleGameMode =
   let filename = "Easy.txt" in getWord filename
+
+--These are SCC (Set Cost Center) to help with performance profiling of the easyWordleGameMode function
 {-# SCC easyWordleGameMode #-}
 
+--the filename for the medium mode
 mediumWordleGameMode:: IO ()
 mediumWordleGameMode =
   let filename = "Medium.txt" in getWord filename
+
+--These are SCC (Set Cost Center) to help with performance profiling of the mediumWordleGameMode function
 {-# SCC mediumWordleGameMode #-}
 
+
+--the filename for the hard mode
 hardWordleGameMode:: IO ()
 hardWordleGameMode = let filename = "Hard.txt" in getWord filename
+
+--These are SCC (Set Cost Center) to help with performance profiling of the hardWordleGameMode function
 {-# SCC hardWordleGameMode #-}
 
-
+--This if the initial entry function for the game
+--the user is given 4 choices to choose from and based on the entry will be taken through the specific mode
+--If the user enters choice 4 it calls the WordleGameMode function with a number 0 which will make it exit
 wordleGameMode :: Int -> IO [Int]
 wordleGameMode num = do
   if num > 0 then do
@@ -166,4 +186,6 @@ wordleGameMode num = do
                wordleGameMode 1
   else
      exitFailure
+
+--These are SCC (Set Cost Center) to help with performance profiling of the wordleGameMode function
 {-# SCC wordleGameMode #-}
